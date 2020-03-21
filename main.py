f07bc3a0fe5557619bfab218302651100a48ea54
David Sun <ds833@cornell.edu>
2020-03-21 17:48:39 -0400

    Added the game loop and main function from board.py module

---
+++ main.py
@@ -0,0 +1,40 @@
+import board as bd
+import minimax
+
+BOARD_WIDTH = 3
+
+game_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+is_won = False
+is_tie = False
+moves_remaining = BOARD_WIDTH ** 2
+
+# TODO: Document game_loop
+
+
+def game_loop(is_player1, board):
+    global is_won, is_tie, moves_remaining
+    while not (is_won or is_tie):
+        bd.print_board(board)
+        pos = bd.get_input(board)
+        bd.place_piece(board, pos, is_player1)
+        moves_remaining = moves_remaining - 1
+        is_won = bd.check_win(board, pos, is_player1)
+        is_tie = (not is_won) and (moves_remaining == 0)
+        if (is_won or is_tie):
+            break
+        is_player1 = not is_player1
+    bd.print_board(board)
+    if is_tie:
+        print("TIE!")
+    else:
+        player = 1 if is_player1 else 2
+        print("Player %d is the winner!" % player)
+
+
+def main():
+    is_p1 = True
+    game_loop(is_p1, game_board)
+
+
+if __name__ == "__main__":
+    main()
