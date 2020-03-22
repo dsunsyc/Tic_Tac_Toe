c75267c5d644db3e622cf1602c3c8f5144060843
David Sun <ds833@cornell.edu>
2020-03-22 00:05:16 -0400

    Began test suite

---
+++ tests.py
@@ -0,0 +1,15 @@
+# import board
+# import minimax
+# import main
+
+# import unittest
+
+# class TestBoardMethods(unittest.TestCase):
+
+#     def test_
+#     is_empty_cell
+#     is_valid
+#     place_piece
+#     remove_piece
+#     check_win
+#     possible_moves
