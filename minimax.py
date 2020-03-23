3d71d375bb22b985fb085ec8f937e49a4fed2947
David Sun <ds833@cornell.edu>
2020-03-23 15:39:25 -0400

    Refactored methods and introduced helper functions for min_move and max_move

--- minimax.py
+++ minimax.py
@@ -9,75 +9,67 @@ TIE = 0
 # higher moves left is a better win. Intuition is to prioritize winning methods
 # that take less steps. Later think of extending losing to take longer maybe?
 
+# TODO: Different Levels of AI
+
+
+def max_helper(cur_score, cur_mv, max_val, max_mv):
+    if cur_score > max_val:
+        max_val = cur_score
+        max_mv = cur_mv
+    return (max_val, max_mv)
+
+
 # max_move(board, moves_left) is a tuple [(a,b)] where [a] is > 0 if the game is
 # winnable by player 1, 0 if tie, and <0 otherwise. [b] is the move that will
 # lead to that result
-
 
 def max_move(board, moves_left):
     max_val = -sys.maxsize - 1
     max_mv = None
     next_moves = bd.possible_moves(board)
-    # need to consider the no possible move situation (full board state) CHECK FOR NONE
     for mv in next_moves:
         bd.place_piece(board, mv, True)
-        # bd.print_board(board)
+        cur_val = None
         if bd.check_win(board, mv, True):
-            if X_WIN > max_val:
-                max_val = X_WIN
-                max_mv = mv
+            cur_val = X_WIN
         elif bd.check_win(board, mv, False):
-            if X_LOSE > max_val:
-                max_val = X_LOSE
-                max_mv = mv
+            cur_val = X_LOSE
         elif moves_left == 1:
-            if TIE > max_val:
-                max_val = TIE
-                max_mv = mv
+            cur_val = TIE
         else:
-            min_tple = min_move(board, moves_left - 1)
-            if min_tple[0] > max_val:
-                max_val = min_tple[0]
-                max_mv = mv
+            cur_val = min_move(board, moves_left - 1)[0]
+        max_val, max_mv = max_helper(cur_val, mv, max_val, max_mv)
         bd.remove_piece(board, mv)
     return (max_val, max_mv)
 
 
+def min_helper(cur_score, cur_mv, min_val, min_mv):
+    if cur_score < min_val:
+        min_val = cur_score
+        min_mv = cur_mv
+    return (min_val, min_mv)
 # min_move(board, moves_left) is a tuple [(a,b)] where [a] is > 0 if the game is
 # winnable by player 2, 0 if tie, and < 0 otherwise. [b] is the move that will
 # lead to that result
-# P2 CURRENTLY sometimes LOSES
+
+
 def min_move(board, moves_left):
-    print("Minimizer")
     min_val = sys.maxsize
     min_mv = None
     next_moves = bd.possible_moves(board)
     for mv in next_moves:
         bd.place_piece(board, mv, False)
-        # bd.print_board(board)
+        cur_val = None
         if bd.check_win(board, mv, False):
-            # print("I made it to p2 winning")
-            if X_LOSE < min_val:
-                min_val = X_LOSE
-                min_mv = mv
+            cur_val = X_LOSE
         elif bd.check_win(board, mv, True):
-            # print("I made it to p1 winning")
-            if X_WIN < min_val:
-                min_val = X_WIN
-                min_mv = mv
+            cur_val = X_WIN
         elif moves_left == 1:
-            if TIE < min_val:
-                min_val = TIE
-                min_mv = mv
+            cur_val = TIE
         else:
-            # print("I made it to the else")
-            max_tple = max_move(board, moves_left - 1)
-            if max_tple[0] < min_val:
-                min_val = max_tple[0]
-                min_mv = mv
+            cur_val = max_move(board, moves_left - 1)[0]
+        min_val, min_mv = min_helper(cur_val, mv, min_val, min_mv)
         bd.remove_piece(board, mv)
-        # print("FOR DEBUGGING: min_val: %d" % min_val)
-        # bd.print_board(board)
     return (min_val, min_mv)
 
 # for all valid moves, check the minimax value. set the best move pointer to be
@@ -85,8 +77,6 @@ def min_move(board, moves_left):
 
 
 def best_move(board, is_max, moves_left):
-    # if not is_max:
-    #     # print("Is minimizer")
     res = max_move(board, moves_left) if is_max else min_move(
         board, moves_left)
     return res[1]
